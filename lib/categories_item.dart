7f28a60b1e6899f3aebff5ec10c799c72cec7f33
ashwinkumars <ashwinkumar.s@inxsasia.com>
2021-11-25 15:29:14 +0530

    first commit

---
+++ lib/categories_item.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'contact_data_model.dart';
+
+class CategoreisItem extends StatelessWidget {
+  late List<Contact> data;
+
+  CategoreisItem({required this.data, key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(top: 15),
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            child: ListView.builder(
+                itemCount: 25,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10)),
+                      color: Colors.white,
+                    ),
+                    width: double.infinity,
+                    height: 80,
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 20),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 20),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                data[index].name,
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              ),
+                              const SizedBox(
+                                height: 6,
+                              ),
+                              // Row(
+                              //   children: <Widget>[
+                              //     const SizedBox(
+                              //       width: 5,
+                              //     ),
+                              //     Text(ContactData[index]['id'],
+                              //         style: const TextStyle(
+                              //             color: Colors.black,
+                              //             fontSize: 13,
+                              //             letterSpacing: .3)),
+                              //   ],
+                              // ),
+                              // const SizedBox(
+                              //   height: 6,
+                              // ),
+                              Row(
+                                children: <Widget>[
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(data[index].contacts,
+                                      style: const TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 13,
+                                          letterSpacing: .3)),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Center(
+                          child: Container(
+                              width: 50,
+                              height: 50,
+                              margin: const EdgeInsets.only(left: 15),
+                              // child: Text('image'
+                              child: Image.network(data[index].url)),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'categoreis_screen.dart';
+import 'tab_bar.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TabBarScreen(),
+    );
+  }
+}
