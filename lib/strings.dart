23525aee2bfb5f6e698e22d8540ffc7e58de70f5
ashwinkumars <ashwinkumar.s@inxsasia.com>
2021-11-26 17:03:52 +0530

    code_updation

---
+++ lib/strings.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Strings {
+  static const String Apptitle = "Watchlist";
+  static const List<Tab> name = <Tab>[
+    Tab(text: "Contact 1"),
+    Tab(text: "Contact 2"),
+    Tab(
+      text: 'Contact 3',
+    )
+  ];
+  static const contact_list_url =
+      'https://5e53a76a31b9970014cf7c8c.mockapi.io/msf/getContacts';
+}
